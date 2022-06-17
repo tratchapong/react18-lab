85e9fa043bf9bf297f8707efcb39a03ce1347ef3
tratchapong <tratchapong@gmail.com>
2022-06-17 08:00:52 +0700

    add SASS/scss bootstrap customization

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import BSButton from "./BSButton";
+import Card from "./Card";
 
 function App() {
   return (
     <div className="App">
       <BSButton />
+      <Card />
     </div>
   );
 }
---
+++ src/Card.jsx
@@ -0,0 +1,121 @@
+import React from "react";
+
+function Card() {
+  {/* <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 w-80 mx-auto">  */}
+  return (
+      <div className="row row-cols-4 w-90 mx-auto" >
+      <div className="col" style={{minWidth: '250px'}}>
+        <div className="card" >
+          <img src="https://picsum.photos/400" className="card-img-top" />
+          <div className="card-body">
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col" style={{minWidth: '250px'}}>
+        <div className="card"   >
+          <img src="https://picsum.photos/400" className="card-img-top" />
+          <div className="card-body">
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col" style={{minWidth: '250px'}}>
+        <div className="card"   >
+          <img src="https://picsum.photos/400" className="card-img-top" />
+          <div className="card-body">
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col" style={{minWidth: '250px'}}>
+        <div className="card"   >
+          <img src="https://picsum.photos/400" className="card-img-top" />
+          <div className="card-body">
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col" style={{minWidth: '250px'}}>
+        <div className="card"   >
+          <img src="https://picsum.photos/400" className="card-img-top" />
+          <div className="card-body">
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col" style={{minWidth: '250px'}}>
+        <div className="card"   >
+          <img src="https://picsum.photos/400" className="card-img-top" />
+          <div className="card-body">
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col" style={{minWidth: '250px'}}>
+        <div className="card"   >
+          <img src="https://picsum.photos/400" className="card-img-top" />
+          <div className="card-body">
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col" style={{minWidth: '250px'}}>
+        <div className="card"   >
+          <img src="https://picsum.photos/400" className="card-img-top" />
+          <div className="card-body">
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col" style={{minWidth: '250px'}}>
+        <div className="card"   >
+          <img src="https://picsum.photos/400" className="card-img-top" />
+          <div className="card-body">
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="col" style={{minWidth: '250px'}}>
+        <div className="card"   >
+          <img src="https://picsum.photos/400" className="card-img-top" />
+          <div className="card-body">
+            <p className="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
